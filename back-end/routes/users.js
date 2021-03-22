f6f4c923ce0cb30dac30282d8e77cfe19670c5ce
Brian Shapiro <briandavidshapiro@gmail.com>
2021-03-22 16:56:08 -0400

    basic tests for users routes written and passing

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -17,7 +17,6 @@ router.get("/", async function (req, res, next) {
 router.get("/:username", async function (req, res, next) {
 	try {
 		const user = await User.findOne(req.params.username);
-		console.log(user);
 		return res.json(user);
 	} catch (e) {
 		return next(e);
@@ -26,7 +25,6 @@ router.get("/:username", async function (req, res, next) {
 
 router.patch("/:username", async function (req, res, next) {
 	try {
-		console.log(req.body);
 		const resp = await User.editProfile(req.body, req.params.username);
 		return res.json(resp);
 	} catch (e) {
@@ -37,7 +35,6 @@ router.patch("/:username", async function (req, res, next) {
 router.post("/bookmarkRecipe", async function (req, res, next) {
 	try {
 		const { username, recipeId } = req.body;
-		console.log(username, recipeId);
 		const resp = await User.bookmarkRecipe(username, recipeId);
 		return res.json(resp);
 	} catch (e) {
@@ -67,7 +64,6 @@ router.get("/:username/getAllBookmarks", async function (req, res, next) {
 router.get("/:username/getEatenMeals", async function (req, res, next) {
 	try {
 		const meals = await User.getEatenMeals(req.params.username, req.query.date);
-		console.log(meals);
 		return res.json(meals);
 	} catch (e) {
 		return next(e);
@@ -77,9 +73,7 @@ router.get("/:username/getEatenMeals", async function (req, res, next) {
 router.post("/addEatenMeal", async function (req, res, next) {
 	try {
 		const { username, recipeId, date } = req.body;
-		console.log(username, recipeId);
 		const resp = await User.addEatenMeal(username, recipeId, date);
-		console.log(resp);
 		return res.json(resp);
 	} catch (e) {
 		return next(e);
--- back-end/routes/users.test.js
+++ back-end/routes/users.test.js
@@ -1,27 +1,217 @@
+process.env.NODE_ENV === "test";
+
+const app = require("../app");
+const request = require("supertest");
+const User = require("../models/user");
+const axios = require("axios");
+const { Client } = require("pg");
+const { DB_URI } = require("../config");
+
+let db = new Client({
+	connectionString: DB_URI,
+});
+
+db.connect();
+
+const user1 = {
+	username: "username1",
+	password: "password1",
+	email: "email1@gmail.com",
+	first_name: "Bob",
+	last_name: "Smith",
+	weight: 170,
+	weight_goal: 160,
+	calorie_goal: 2000,
+};
+const user1EditData = {
+	email: "email12345@gmail.com",
+	first_name: "Bobby",
+	last_name: "Smitherson",
+	weight: 175,
+	weight_goal: 160,
+	calorie_goal: 1900,
+};
+const user2 = {
+	username: "username2",
+	password: "password2",
+	email: "email2@gmail.com",
+	first_name: "Jane",
+	last_name: "Doe",
+	weight: 130,
+	weight_goal: 115,
+	calorie_goal: 1700,
+};
+const bookmarks1 = [716627, 716408, 716426, 715594, 715497];
+
+const eatenMeals1 = {
+	"2021-03-14": [652423, 660306],
+	"2021-03-15": [715424, 662670],
+	"2021-03-16": [716195, 663559],
+	"2021-03-17": [633942, 715521],
+};
+
+const addBookmarks = async (bookmarks, user) => {
+	for (let id of bookmarks) {
+		await User.bookmarkRecipe(user.username, id);
+	}
+};
+
+const addEatenMeals = async (eatenMeals, user) => {
+	for (let date in eatenMeals) {
+		for (let id of eatenMeals[date]) {
+			await User.addEatenMeal(user.username, id, date);
+		}
+	}
+};
+
+beforeEach(async () => {
+	await db.query("DELETE FROM users_meals");
+	await db.query("DELETE FROM bookmarks");
+	await db.query("DELETE FROM users");
+	await User.register(user1);
+});
+
+// afterEach(async () => {
+// 	await db.query("DELETE FROM users_meals");
+// 	await db.query("DELETE FROM bookmarks");
+// 	await db.query("DELETE FROM users");
+// });
+
 describe("GET / route", () => {
-	it("should return JSON of all users", () => {});
+	it("should return JSON of all users", async () => {
+		await User.register(user2);
+		const resp = await request(app).get("/users");
+		expect(resp.body).toEqual([
+			{
+				username: user1.username,
+				email: user1.email,
+				first_name: user1.first_name,
+				last_name: user1.last_name,
+			},
+			{
+				username: user2.username,
+				email: user2.email,
+				first_name: user2.first_name,
+				last_name: user2.last_name,
+			},
+		]);
+	});
 });
 describe("GET /:username route", () => {
-	it("should return information on a single user", () => {});
+	it("should return information on a single user", async () => {
+		const resp = await request(app).get(`/users/${user1.username}`);
+		expect(resp.body).toEqual({
+			username: user1.username,
+			email: user1.email,
+			first_name: user1.first_name,
+			last_name: user1.last_name,
+			bookmarks: [],
+			eatenMeals: {},
+		});
+	});
 });
 describe("PATCH /:username route", () => {
-	it("should update information on a single user", () => {});
+	it("should update information on a single user", async () => {
+		const resp = await request(app)
+			.patch(`/users/${user1.username}`)
+			.send(user1EditData);
+
+		expect(resp.body).toEqual({
+			username: user1.username,
+			email: user1EditData.email,
+			first_name: user1EditData.first_name,
+			last_name: user1EditData.last_name,
+			weight: user1EditData.weight,
+			weight_goal: user1EditData.weight_goal,
+			calorie_goal: user1EditData.calorie_goal,
+			bookmarks: [],
+			eatenMeals: {},
+		});
+	});
 });
 describe("POST /bookmarkRecipe route", () => {
-	it("should add a recipe to a user's bookmarks", () => {});
+	it("should add a recipe to a user's bookmarks", async () => {
+		const resp = await request(app)
+			.post(`/users/bookmarkRecipe`)
+			.send({ username: user1.username, recipeId: bookmarks1[1] });
+		expect(resp.body).toEqual({ message: "Bookmarked recipe 716408" });
+
+		const user = await db.query(
+			`SELECT meal_id FROM bookmarks WHERE username=$1`,
+			[user1.username]
+		);
+		expect(user.rows[0]).toEqual({ meal_id: 716408 });
+	});
 });
 describe("POST /unbookmarkRecipe route", () => {
-	it("should remove a recipe from a user's bookmarks", () => {});
+	it("should remove a recipe from a user's bookmarks", async () => {
+		await User.bookmarkRecipe(user1.username, bookmarks1[2]);
+		const resp = await request(app)
+			.post(`/users/unbookmarkRecipe`)
+			.send({ username: user1.username, recipeId: bookmarks1[2] });
+		expect(resp.body).toEqual({ message: "Unbookmarked recipe 716426" });
+
+		const user = await db.query(
+			`SELECT meal_id FROM bookmarks WHERE username=$1`,
+			[user1.username]
+		);
+		expect(user.rows.length).toBe(0);
+	});
 });
 describe("GET /:username/getAllBookmarks route", () => {
-	it("should get all recipes that a user has bookmarked", () => {});
+	it("should get all recipes that a user has bookmarked", async () => {
+		await addBookmarks(bookmarks1, user1);
+		const resp = await request(app).get(
+			`/users/${user1.username}/getAllBookmarks`
+		);
+		expect(resp.body).toEqual(bookmarks1);
+	});
 });
 describe("GET /:username/getEatenMeals route", () => {
-	it("should get all meals that a user has eaten", () => {});
+	it("should get all meals that a user has eaten for a specific date", async () => {
+		await addEatenMeals(eatenMeals1, user1);
+		for (let date in eatenMeals1) {
+			const resp = await request(app).get(
+				`/users/${user1.username}/getEatenMeals?date=${date}`
+			);
+			expect(resp.body).toEqual(eatenMeals1[date]);
+		}
+	});
 });
 describe("POST /addEatenMeal route", () => {
-	it("should add a meal to a user's eaten meals", () => {});
+	it("should add a meal to a user's eaten meals", async () => {
+		const resp = await request(app).post(`/users/addEatenMeal`).send({
+			username: user1.username,
+			recipeId: bookmarks1[0],
+			date: "2021-03-14",
+		});
+
+		expect(resp.body).toEqual({ message: "Meal eaten" });
+
+		const user = await User.findOne(user1.username);
+
+		expect(Object.keys(user.eatenMeals)).toContain("2021-03-14");
+		expect(user.eatenMeals["2021-03-14"]).toEqual([716627]);
+	});
 });
 describe("POST /removeEatenMeal route", () => {
-	it("should add a meal to a user's eaten meals", () => {});
+	it("should remove a meal from a user's eaten meals", async () => {
+		await User.addEatenMeal(user1.username, bookmarks1[0], "2021-03-14");
+
+		const resp = await request(app).post(`/users/removeEatenMeal`).send({
+			username: user1.username,
+			recipeId: bookmarks1[0],
+			date: "2021-03-14",
+		});
+
+		expect(resp.body).toEqual({ message: "Meal deleted" });
+
+		const user = await User.findOne(user1.username);
+
+		expect(Object.keys(user.eatenMeals)).not.toContain("2021-03-14");
+	});
+});
+
+afterAll(async () => {
+	await db.end();
 });
