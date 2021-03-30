b13c8c84af39e7248f9425e78587cedfa394da73
Brian Shapiro <briandavidshapiro@gmail.com>
2021-03-30 15:32:19 -0400

    backend working

--- config.js
+++ config.js
@@ -8,6 +8,8 @@ if (process.env.NODE_ENV === "test") {
 	DB_URI = process.env.DATABASE_URI || `${DB_URI}/kitchen_menu`;
 }
 
+console.log(process.env.SECRET_KEY);
+
 const BCRYPT_WORK_FACTOR = 12;
 const SECRET_KEY = process.env.SECRET_KEY;
 module.exports = { DB_URI, BCRYPT_WORK_FACTOR, SECRET_KEY };
