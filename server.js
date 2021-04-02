53e20c1a7cc9f3af5d9ad6667aeacb2afd7b75e0
Brian Shapiro <briandavidshapiro@gmail.com>
2021-04-02 13:14:10 -0400

    updated port in server.js

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get("*", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
 	console.log("Server listening on port 5000.");
 });
