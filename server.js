acd6e2ec0887b7d4399b19f39c046683908b054b
Brian Shapiro <briandavidshapiro@gmail.com>
2021-04-02 14:00:30 -0400

    trying to fix 404

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const path = require("path");
 const express = require("express");
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "../front-end/build")));
 
 // Handle GET requests to /api route
-app.get("/api", (req, res) => {
-	res.json({ message: "Hello from server!" });
-});
+// app.get("/api", (req, res) => {
+// 	res.json({ message: "Hello from server!" });
+// });
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+	res.sendFile(path.resolve(__dirname, "../front-end/build", "index.html"));
 });
 
 app.listen(process.env.PORT || 5000, () => {
