41e5e284b801c245f8718fc0dfa647727a88c0a5
Brian Shapiro <briandavidshapiro@gmail.com>
2021-04-02 17:14:55 -0400

    moved server.js to app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const authRoutes = require("./routes/auth");
 const express = require("express");
 const ExpressError = require("./expressError");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -31,6 +32,23 @@ app.use(function (err, req, res, next) {
 		error: err,
 		message: err.message,
 	});
+});
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "./front-end/build")));
+
+// Handle GET requests to /api route
+// app.get("/api", (req, res) => {
+// 	res.json({ message: "Hello from server!" });
+// });
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./front-end/build", "index.html"));
+});
+
+app.listen(process.env.PORT || 5000, () => {
+	console.log("Server listening on port 5000.");
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const app = require("./app");
+// const app = require("./app");
 // const path = require("path");
 // const express = require("express");
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "./front-end/build")));
+// app.use(express.static(path.resolve(__dirname, "./front-end/build")));
 
 // Handle GET requests to /api route
 // app.get("/api", (req, res) => {
@@ -11,10 +11,10 @@ app.use(express.static(path.resolve(__dirname, "./front-end/build")));
 // });
 
 // All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "./front-end/build", "index.html"));
-});
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.resolve(__dirname, "./front-end/build", "index.html"));
+// });
 
-app.listen(process.env.PORT || 5000, () => {
-	console.log("Server listening on port 5000.");
-});
+// app.listen(process.env.PORT || 5000, () => {
+// 	console.log("Server listening on port 5000.");
+// });
