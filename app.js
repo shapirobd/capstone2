cd8cac60305dabe69ab3723a46f1a7f16d0cd740
Brian Shapiro <briandavidshapiro@gmail.com>
2021-04-03 12:31:40 -0400

    trying to fix 404

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const path = require("path");
 
 const app = express();
 
+const port = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use(cors());
 
@@ -14,15 +16,26 @@ app.use("/users", usersRoutes);
 app.use("/auth", authRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+	app.use(express.static(path.join(__dirname, "front-end/build")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join((__dirname = "front-end/build/index.html")));
+	});
 }
 
-// 404 handler
-app.use(function (req, res, next) {
-	console.log(req);
-	const err = new ExpressError(`Not found`, 404);
-	return next(err);
+// Have Node serve the files for our built React app
+app.use(express.static(path.join(__dirname, "front-end/build")));
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname + "/front-end/public/index.html"));
 });
+
+// 404 handler
+// app.use(function (req, res, next) {
+// 	console.log(req);
+// 	const err = new ExpressError(`Not found`, 404);
+// 	return next(err);
+// });
 
 // general error handler
 app.use(function (err, req, res, next) {
@@ -34,21 +47,8 @@ app.use(function (err, req, res, next) {
 	});
 });
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "./front-end/build")));
-
-// Handle GET requests to /api route
-// app.get("/api", (req, res) => {
-// 	res.json({ message: "Hello from server!" });
-// });
-
-// All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname + "/front-end/build/index.html"));
-});
-
-app.listen(process.env.PORT || 5000, () => {
-	console.log("Server listening on port 5000.");
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}.`);
 });
 
 module.exports = app;
